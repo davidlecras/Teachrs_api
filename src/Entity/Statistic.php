ba9f12b2e32b5be4c7eb5f70b31356683ae29be3
David <davidlecras@hotmail.com>
2021-02-22 15:17:21 +0100

    inital commit

---
+++ src/Entity/Statistic.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StatisticRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StatisticRepository::class)
+ */
+class Statistic
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $count;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCount(): ?int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): self
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+}
